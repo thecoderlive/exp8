adfc417e964f1561d3a5952e82ed551be97100af
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 00:31:33 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "f4bbfbd8-d3f0-400f-823d-6691dac2422c",
+    "home_feed": [
+        {
+            "id": "64ea6539-6656-479f-90c8-be29574debd9",
+            "artist_name": "factor",
+            "home_carousel": [
+                {
+                    "id": "2d8b4d63-e8f6-4cd8-a4a9-c7c7b013aa8b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/254.png"
+                },
+                {
+                    "id": "19cc541d-9392-4934-8f86-9e4c035ecd04",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png"
+                },
+                {
+                    "id": "7b669582-e5a7-4af3-bd6a-9e9205f63367",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png"
+                },
+                {
+                    "id": "332418a3-9b27-44ae-a643-26a67167c0cb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png"
+                },
+                {
+                    "id": "5273c4a2-3283-4730-9a82-72487221b0a4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/59.png"
+                }
+            ],
+            "art_caption": "especially",
+            "art_description": "Also song letter once pull campaign. Onto military start during message local teach third. Type mention method cover ask. Officer dog life hotel."
+        },
+        {
+            "id": "19517d79-72ad-4e9c-949a-2a59df2c390e",
+            "artist_name": "give",
+            "home_carousel": [
+                {
+                    "id": "904ca2ab-d335-4525-bf1b-c2d04a5c34d9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png"
+                },
+                {
+                    "id": "2ddd5c73-4cc4-46fc-93d3-368e67ae088c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                },
+                {
+                    "id": "05027966-1c43-4495-9147-8403dd3a93bc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "b8580371-c7c9-49b9-85db-957401eda408",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "0fb10437-8d3e-4d70-ba76-348e4545d439",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/238.png"
+                }
+            ],
+            "art_caption": "rich",
+            "art_description": "Talk them travel television prove. Own sit area image style about. Assume reduce responsibility manage."
+        },
+        {
+            "id": "22992779-4b6e-4b35-a545-7840ee6b6a0e",
+            "artist_name": "energy",
+            "home_carousel": [
+                {
+                    "id": "b0d07b85-7821-43de-9d03-6ea48eb586ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/119.png"
+                },
+                {
+                    "id": "5c822d70-21b5-4bd4-bc1d-57f5b164e5f7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+                },
+                {
+                    "id": "6763e1e4-dd9a-40e0-8bf7-63175264fc7c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png"
+                },
+                {
+                    "id": "d95b2808-9d4c-4d56-944b-6deb70a2e3b7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png"
+                },
+                {
+                    "id": "23604e1f-007e-4a84-8ff6-09143eba8b34",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                }
+            ],
+            "art_caption": "season",
+            "art_description": "Movement seem so now begin me. Others hotel whether pattern although woman bar form."
+        },
+        {
+            "id": "91cb04c9-7183-4457-ab97-65c837995fa4",
+            "artist_name": "consumer",
+            "home_carousel": [
+                {
+                    "id": "caee7f4f-7fcd-4385-b6c1-c930ac782b96",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "d6064aa2-06c5-4646-8529-87e830ae91c4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "bcfc580a-53e6-44e1-aba9-647519c18ac8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                },
+                {
+                    "id": "3ece74b8-8e9b-4687-848d-cc0a2e2f343c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/62.png"
+                },
+                {
+                    "id": "e3f76cde-c635-4871-a63c-ebca393b6639",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png"
+                }
+            ],
+            "art_caption": "girl",
+            "art_description": "Century determine I value. Memory find name cover way."
+        },
+        {
+            "id": "d686a9a9-96d7-4b3e-9b5d-6d5f37509e8d",
+            "artist_name": "theory",
+            "home_carousel": [
+                {
+                    "id": "2f73ceee-a3ec-446c-bd6e-c8f7e1520a88",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                },
+                {
+                    "id": "f9837b27-491d-435f-a9d0-af1eae05fd07",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/270.png"
+                },
+                {
+                    "id": "f1b87849-d71d-4b2d-ae9c-dbf13589bb83",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/192.png"
+                },
+                {
+                    "id": "1d66db8d-e93c-4201-aef4-a583a63ccc48",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/366.png"
+                },
+                {
+                    "id": "27cc58ce-7ee1-463d-a88f-59515a5e82de",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/43.png"
+                }
+            ],
+            "art_caption": "final",
+            "art_description": "Lot popular interview data anything either subject. Record interesting behavior her skin. Space nor country opportunity. Win close report hair act fall."
+        }
+    ]
+}
